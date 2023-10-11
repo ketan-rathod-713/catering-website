1a86c5b659202518802cbdc1cc7ef3dc52efe80f
ketan <ketanrtd1@gmail.com>
2023-10-11 21:05:53 +0530

    improve mobile navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,8 @@ import navbarItems from "../data/navbarItems";
 
 const Navbar = () => {
   const [openNavbar, setOpenNavbar] = useState(false);
+  const [openSecondBar, setOpenSecondBar] = useState(false);
+  
 
   const toggleNavbar = () => {
     if (openNavbar) {
@@ -13,6 +15,14 @@ const Navbar = () => {
       setOpenNavbar(true);
     }
   };
+
+  const toggleSecondNavbar = ()=>{
+    if(openSecondBar){
+      setOpenSecondBar(false)
+    } else {
+      setOpenSecondBar(true)
+    }
+  }
 
   return (
     <div className="sticky navbar top-0 relative z-20">
@@ -83,11 +93,55 @@ const Navbar = () => {
       </div>
 
       {/* second heading */}
+      <div className="flex lg:hidden justify-center items-center bg-white py-2 font-bold cursor-pointer hover:pt-3 hover:pb-1" onClick={toggleSecondNavbar}>
+      {
+        !openSecondBar
+        ?
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+        :
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+        </svg>
+      }
+      </div>
+
+      {/* for small screens, hidden for large screens*/}
+     {
+      openSecondBar && <motion.div
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 1 }}
+        className="visible lg:hidden flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-10 px-10 py-2 justify-end bg-white"
+      >
+        <Link
+          href={"/gallary"}
+          className="px-6 py-1 bg-green-400 text-gray-900 font-bold shadow-md shadow-red-300"
+        >
+          VIEW GALLARY
+        </Link>
+        <Link
+          href={"/shop/product"}
+          className="px-6 py-1 bg-red-400 text-gray-900 font-bold shadow-md shadow-red-300"
+        >
+          SHOP NOW
+        </Link>
+        <Link
+          href={"/contactus"}
+          className="px-6 py-1 bg-blue-400 text-gray-900 font-bold shadow-md shadow-blue-300"
+        >
+          ENQUIRE NOW
+        </Link>
+      </motion.div>
+     } 
+
+{/* for large screens, it is hidden for small screens */}
       <motion.div
         initial={{ y: -100 }}
         animate={{ y: 0 }}
         transition={{ duration: 1 }}
-        className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-10 px-10 py-2 justify-end bg-white"
+        className="hidden lg:visible lg:flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-10 px-10 py-2 justify-end bg-white"
       >
         <Link
           href={"/gallary"}
